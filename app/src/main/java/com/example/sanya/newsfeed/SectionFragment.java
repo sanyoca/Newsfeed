c355959f3030f90e88c6e81d0194b3a5f31558eb
sanyoca <sanyoca@gmail.com>
2017-06-02 12:20:12 +0200

    V1. 0
    - no tetching nor refetching yet

--- app/src/main/java/com/example/sanya/newsfeed/SectionFragment.java
+++ app/src/main/java/com/example/sanya/newsfeed/SectionFragment.java
@@ -1,6 +1,8 @@
 package com.example.sanya.newsfeed;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -10,12 +12,19 @@ import android.widget.TextView;
 
 public class SectionFragment extends Fragment {
 
+    final static String stringGuardianURL = "https://content.guardianapis.com/search?api-key=test&tag=";
+    final static String stringGuardianPagesize = "&page-size=";
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.sectionlayout, container, false);
 
-        ((TextView)rootView.findViewById(R.id.texthere)).setText(getArguments().getString("section"));
+        SharedPreferences preferredSections = PreferenceManager.getDefaultSharedPreferences(getActivity());
+        preferredSections.getBoolean("interested_sport", false);
+        // TODO: Csinálni a preferences között egy értéket, ami azt tárolja, mennyi cikket mutasson az oldalon. Default: 12.
+        String stringFullURL = stringGuardianURL + getArguments().getString("section") + "/" + getArguments().getString("section") + stringGuardianPagesize + "20";
+        ((TextView)rootView.findViewById(R.id.texthere)).setText(stringFullURL);
         return rootView;
     }
 }
